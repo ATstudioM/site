8da825336273cc431996347ee6fa05dec5578edf
ATstudio <ATstudiorussia@gmail.com>
2022-03-08 14:44:49 +0300

    1

---
+++ hmm.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def page():
+    return render_template('page.html')
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/promotion')
+def promotion():
+    return render_template('promotion.html')
+
+
+@app.route('/image_mars')
+def image_mars():
+    return render_template('image_mars.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
